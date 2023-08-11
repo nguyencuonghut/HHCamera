637600c560160a30461856302a60a62d5b30057d
Tony Ng <nguyencuonghut55@gmal.com>
2023-08-11 16:31:20 +0700

    Add charts for Admin Dashboard

--- app/Http/Controllers/AdminHomeController.php
+++ app/Http/Controllers/AdminHomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Admin;
+use App\Models\Device;
+use App\Models\Error;
+use App\Models\ErrorType;
+use App\Models\Farm;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use RealRashid\SweetAlert\Facades\Alert;
@@ -11,7 +15,52 @@ class AdminHomeController extends Controller
 {
     public function index()
     {
-        return view('admin.home');
+        $farms_cnt = Farm::count();
+        $devices_cnt = Device::count();
+        $errors_cnt = Error::count();
+        $cams_cnt = Device::where('device_category_id', 1)->count();
+        $cam_on_cnt = Device::where('device_category_id', 1)->where('status', 'ON')->count();
+        $cam_off_cnt = Device::where('device_category_id', 1)->where('status', 'OFF')->count();
+
+
+        $error_type_id_1_cnt = Error::where('type_id', 1)->count();
+        $error_type_id_2_cnt = Error::where('type_id', 2)->count();
+        $error_type_id_3_cnt = Error::where('type_id', 3)->count();
+        $error_type_id_4_cnt = Error::where('type_id', 4)->count();
+        $error_type_id_5_cnt = Error::where('type_id', 5)->count();
+        $error_type_id_6_cnt = Error::where('type_id', 6)->count();
+        $error_type_id_7_cnt = Error::where('type_id', 7)->count();
+
+        $error_type_id_1_name = ErrorType::findOrFail(1)->name;
+        $error_type_id_2_name = ErrorType::findOrFail(2)->name;
+        $error_type_id_3_name = ErrorType::findOrFail(3)->name;
+        $error_type_id_4_name = ErrorType::findOrFail(4)->name;
+        $error_type_id_5_name = ErrorType::findOrFail(5)->name;
+        $error_type_id_6_name = ErrorType::findOrFail(6)->name;
+        $error_type_id_7_name = ErrorType::findOrFail(7)->name;
+        return view('admin.home',
+                    [
+                        'farms_cnt' => $farms_cnt,
+                        'devices_cnt' => $devices_cnt,
+                        'errors_cnt' => $errors_cnt,
+                        'cams_cnt' => $cams_cnt,
+                        'cam_on_cnt' => $cam_on_cnt,
+                        'cam_off_cnt' => $cam_off_cnt,
+                        'error_type_id_1_cnt' => $error_type_id_1_cnt,
+                        'error_type_id_2_cnt' => $error_type_id_2_cnt,
+                        'error_type_id_3_cnt' => $error_type_id_3_cnt,
+                        'error_type_id_4_cnt' => $error_type_id_4_cnt,
+                        'error_type_id_5_cnt' => $error_type_id_5_cnt,
+                        'error_type_id_6_cnt' => $error_type_id_6_cnt,
+                        'error_type_id_7_cnt' => $error_type_id_7_cnt,
+                        'error_type_id_1_name' => $error_type_id_1_name,
+                        'error_type_id_2_name' => $error_type_id_2_name,
+                        'error_type_id_3_name' => $error_type_id_3_name,
+                        'error_type_id_4_name' => $error_type_id_4_name,
+                        'error_type_id_5_name' => $error_type_id_5_name,
+                        'error_type_id_6_name' => $error_type_id_6_name,
+                        'error_type_id_7_name' => $error_type_id_7_name,
+                    ]);
     }
 
 
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -11,11 +11,11 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Admin Dashboard</h1>
+            <h1 class="m-0">Dashboard</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item active">Admin Dashboard</li>
+              <li class="breadcrumb-item active">Dashboard</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
@@ -32,14 +32,14 @@
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>150</h3>
+                <h3>{{$farms_cnt}}</h3>
 
-                <p>Tổng số tender</p>
+                <p>Tổng số trại</p>
               </div>
               <div class="icon">
                 <i class="ion ion-ios-copy"></i>
               </div>
-              <a href="#" class="small-box-footer">Xem thêm <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{route('admin.farms.index')}}" class="small-box-footer">Xem thêm <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -47,14 +47,14 @@
             <!-- small box -->
             <div class="small-box bg-success">
               <div class="inner">
-                <h3>53</h3>
+                <h3>{{$devices_cnt}}</h3>
 
-                <p>Tender hoàn thành</p>
+                <p>Tổng số thiết bị</p>
               </div>
               <div class="icon">
                 <i class="ion ion-checkmark-circled"></i>
               </div>
-              <a href="#" class="small-box-footer">Xem thêm <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{route('admin.devices.index')}}" class="small-box-footer">Xem thêm <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -62,14 +62,14 @@
             <!-- small box -->
             <div class="small-box bg-warning">
               <div class="inner">
-                <h3>44</h3>
+                <h3>{{$cams_cnt}}</h3>
 
-                <p>Tender đang diễn ra</p>
+                <p>Tổng số cam</p>
               </div>
               <div class="icon">
                 <i class="ion ion-load-a"></i>
               </div>
-              <a href="#" class="small-box-footer">Xem thêm <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{route('admin.devices.index')}}" class="small-box-footer">Xem thêm <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -77,21 +77,152 @@
             <!-- small box -->
             <div class="small-box bg-danger">
               <div class="inner">
-                <h3>65</h3>
+                <h3>{{$errors_cnt}}</h3>
 
-                <p>Số nhà cung cấp</p>
+                <p>Tổng số lỗi</p>
               </div>
               <div class="icon">
                 <i class="ion ion-pie-graph"></i>
               </div>
-              <a href="#" class="small-box-footer">Xem thêm <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{route('admin.errors.index')}}" class="small-box-footer">Xem thêm <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
         </div>
         <!-- /.row -->
+        <div class="row">
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title">
+                            <b style="color:##212529;">TỔNG HỢP CAMERA</b>
+                            <span class="badge bg-success">ON</span> {{$cam_on_cnt}}
+                            &nbsp;
+                            <span class="badge bg-danger">OFF</span> {{$cam_off_cnt}}
+                        </h5>
+                    </div>
+
+                    <div class="card-body">
+                    <canvas id="donutCamChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title">
+                            <b style="color:##212529;">TỔNG HỢP LỖI</b>
+                        </h5>
+                    </div>
+
+                    <div class="card-body">
+                    <canvas id="donutErrorChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                    </div>
+                </div>
+            </div>
+          </div>
       </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
   </div>
 @endsection
+
+
+@push('scripts')
+<!-- ChartJS -->
+<script src="{{ asset('plugins/chart.js/Chart.min.js') }}"></script>
+
+<script>
+    //-------------
+    //- DONUT CAM CHART -
+    //-------------
+    // Get context with jQuery - using jQuery's .get() method.
+    var donutCamChartCanvas = $('#donutCamChart').get(0).getContext('2d')
+    var cam_on_cnt =  {{ Js::from($cam_on_cnt) }};
+    var cam_off_cnt =  {{ Js::from($cam_off_cnt) }};
+    var donutData        = {
+      labels: [
+          'ON',
+          'OFF',
+      ],
+      datasets: [
+        {
+          data: [cam_on_cnt,cam_off_cnt],
+          backgroundColor : ['#00a65a', '#f56954'],
+        }
+      ]
+    }
+    var donutOptions     = {
+      maintainAspectRatio : false,
+      responsive : true,
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    new Chart(donutCamChartCanvas, {
+      type: 'doughnut',
+      data: donutData,
+      options: donutOptions,
+      display: true
+    })
+
+
+    //-------------
+    //- DONUT ERROR CHART -
+    //-------------
+    // Get context with jQuery - using jQuery's .get() method.
+    var donutErrorChartCanvas = $('#donutErrorChart').get(0).getContext('2d')
+    var error_type_id_1_cnt =  {{ Js::from($error_type_id_1_cnt) }};
+    var error_type_id_2_cnt =  {{ Js::from($error_type_id_2_cnt) }};
+    var error_type_id_3_cnt =  {{ Js::from($error_type_id_3_cnt) }};
+    var error_type_id_4_cnt =  {{ Js::from($error_type_id_4_cnt) }};
+    var error_type_id_5_cnt =  {{ Js::from($error_type_id_5_cnt) }};
+    var error_type_id_6_cnt =  {{ Js::from($error_type_id_6_cnt) }};
+    var error_type_id_7_cnt =  {{ Js::from($error_type_id_7_cnt) }};
+
+    var error_type_id_1_name = {{ Js::from($error_type_id_1_name) }};
+    var error_type_id_2_name = {{ Js::from($error_type_id_2_name) }};
+    var error_type_id_3_name = {{ Js::from($error_type_id_3_name) }};
+    var error_type_id_4_name = {{ Js::from($error_type_id_4_name) }};
+    var error_type_id_5_name = {{ Js::from($error_type_id_5_name) }};
+    var error_type_id_6_name = {{ Js::from($error_type_id_6_name) }};
+    var error_type_id_7_name = {{ Js::from($error_type_id_7_name) }};
+    var donutData        = {
+      labels: [
+        error_type_id_1_name,
+        error_type_id_2_name,
+        error_type_id_3_name,
+        error_type_id_4_name,
+        error_type_id_5_name,
+        error_type_id_6_name,
+        error_type_id_7_name,
+      ],
+      datasets: [
+        {
+          data: [
+            error_type_id_1_cnt,
+            error_type_id_2_cnt,
+            error_type_id_3_cnt,
+            error_type_id_4_cnt,
+            error_type_id_5_cnt,
+            error_type_id_6_cnt,
+            error_type_id_7_cnt,
+        ],
+          backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de', '#02fc4d'],
+        }
+      ]
+    }
+    var donutOptions     = {
+      maintainAspectRatio : false,
+      responsive : true,
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    new Chart(donutErrorChartCanvas, {
+      type: 'doughnut',
+      data: donutData,
+      options: donutOptions,
+    })
+
+  </script>
+@endpush
+
